4be0c791418f9f90fe9b7f92d761130d4ac7db79
Wee Hean Ng <heanconcerto@gmail.com>
2023-03-18 16:18:12 +0800

    Ensured predictor.py conform to pylint

--- predictor.py
+++ predictor.py
@@ -1,24 +1,26 @@
+"""This module asks user for input mileage and provide a price estimate."""
+
 import csv
 
 from trainer import predict
 
 
 if __name__ == "__main__":
+    INFILE = "coefficient.csv"
+
     theta = [0.0, 0.0]
-    
-    infile = "coefficient.csv"
     try:
-        f = open(infile)
+        f = open(INFILE, encoding="utf-8")
     except FileNotFoundError:
-        print(f"{infile} not found. Coefficient defaults to 0.")
+        print(f"{INFILE} not found. Coefficient defaults to 0.")
     else:
         # read only 1 row from the file
         reader = csv.DictReader(f)
         row = next(reader)
         theta = [float(row["theta" + str(i)]) for i in range(len(row))]
         f.close()
-    
-    # prompt user 
+
+    # prompt user
     mileage = float(input("Please enter mileage: "))
     price = predict(theta, mileage)
     print(f"The predicted price is {price:.0f}.")
